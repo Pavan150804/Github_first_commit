b4e78a87616155421f8fe34f76049e30200eb5ab
Pavan150804 <p.pavanmanikanta12@gmail.com>
2026-08-04 14:29:37 +0530

    OrderPage modified

--- src/tests/OrderTest.java
+++ src/tests/OrderTest.java
@@ -4,5 +4,9 @@ public class OrderTest {
 	public static void main(String[] args) {
 		System.out.println("order test cases..");
 	}
+	
+	public static void passed() {
+		System.out.println("All are passed..");
+	}
 
 }
