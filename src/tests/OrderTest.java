9d759857d9357c7f5c32e476941e65c9e5c636a5
Pavan150804 <p.pavanmanikanta12@gmail.com>
2026-08-05 12:02:41 +0530

    Merge master into cart

---
+++ src/tests/OrderTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class OrderTest {
+	public static void main(String[] args) {
+		System.out.println("order test cases..");
+	}
+	
+	public static void passed() {
+		System.out.println("All are passed..");
+	}
+
+}
