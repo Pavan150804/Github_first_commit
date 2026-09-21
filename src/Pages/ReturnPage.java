de63accbb9ef3254fd8eca93659a89529507ec53
Pavan150804 <p.pavanmanikanta12@gmail.com>
2026-09-21 12:19:29 +0530

    dev1 changes are commited

--- src/Pages/ReturnPage.java
+++ src/Pages/ReturnPage.java
@@ -3,13 +3,10 @@ package Pages;
 public class ReturnPage {
 
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
-		String namedev2="return Initiated";
-		System.out.println(namedev2);
-=======
+
 		String namedev1="return Initiated";
 		System.out.println(namedev1);
->>>>>>> Stashed changes
+
 
 	}
 
