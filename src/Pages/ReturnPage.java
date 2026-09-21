e3521a50ce00ce2a5a89ad8a951d340d3e5bce1a
Pavan150804 <p.pavanmanikanta12@gmail.com>
2026-09-21 12:18:52 +0530

    dev 1 commit

--- src/Pages/ReturnPage.java
+++ src/Pages/ReturnPage.java
@@ -3,8 +3,13 @@ package Pages;
 public class ReturnPage {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		String namedev2="return Initiated";
 		System.out.println(namedev2);
+=======
+		String namedev1="return Initiated";
+		System.out.println(namedev1);
+>>>>>>> Stashed changes
 
 	}
 
