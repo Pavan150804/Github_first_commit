e4d3ef693b9e1699c82db3d7ec7f1a9d117326e0
Pavan150804 <p.pavanmanikanta12@gmail.com>
2026-09-21 11:16:28 +0530

    third cherry commit

--- src/Pages/Cherrypick.java
+++ src/Pages/Cherrypick.java
@@ -5,7 +5,10 @@ public class Cherrypick {
 	public static void main(String[] args) {
 		String name="Pavan";
 		int salary=20000;
-
+	}
+	
+	public void hai() {
+		System.out.println("hai....");
 	}
 
 }
